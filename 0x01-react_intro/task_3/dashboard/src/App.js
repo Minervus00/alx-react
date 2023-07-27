e617570f1cb37b0762f78098133a572492190291
Minervus00 <fayouedraogo@protonmail.com>
2023-07-27 02:27:02 +0000

    fun

--- 0x01-react_intro/task_3/dashboard/src/App.js
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import logo from './holberton_logo.jpg';
+import { getFullYear, getFooterCopy } from './utils';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="App-header">
+        <img src={logo} alt='logo' />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        <input type='text' id='email'></input>
+        <label htmlFor="password">Password:</label>
+        <input type='text' id='password'></input>
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </div>
     </div>
   );
 }
