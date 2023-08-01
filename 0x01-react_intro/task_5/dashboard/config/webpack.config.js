eb4419745664c2b13bab2388a3b50081b2da5604
Minervus00 <fayouedraogo@protonmail.com>
2023-08-01 12:00:42 +0000

    fun

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: path.relative(__dirname, './src/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  mode: 'production',
+  performance: {
+		maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+	},
+  module: {
+    rules: [
+      { 
+        test: /\.css$/, 
+        use: ["style-loader", "css-loader"] 
+      },
+      { 
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            }
+          },
+        ]
+      },
+    ]
+  }
+};
---
+++ 0x01-react_intro/task_5/dashboard/src/Notifications.test.js
@@ -0,0 +1,24 @@
+import { shallow } from 'enzyme';
+import Notifications from './Notifications.js';
+
+it('renders without crashing', () => {
+  const wrapper = shallow(<Notifications />);
+  expect(wrapper).toBeDefined();
+});
+
+it("renders ul", () => {
+  const wrapper = shallow(<Notifications />);
+  expect(wrapper.find("ul")).toBeDefined();
+});
+
+it('renders three list items', () => {
+  const wrapper = shallow(<Notifications />);
+  expect(wrapper.find('li').length).toBe(3);
+});
+
+it('renders the text Here is the list of notifications', () => {
+  const wrapper = shallow(<Notifications />);
+  expect(wrapper.find('p').text()).toBe('Here is the list of notifications');
+});
+
+
