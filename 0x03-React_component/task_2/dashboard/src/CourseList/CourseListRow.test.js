eb37f7c41991b07698d94b9ea6c945e72e32ffe4
Minervus00 <fayouedraogo@protonmail.com>
2023-08-04 20:47:51 +0000

    funny

---
+++ 0x03-React_component/task_2/dashboard/src/CourseList/CourseListRow.test.js
@@ -0,0 +1,20 @@
+import {shallow} from 'enzyme';
+import CourseListRow from './CourseListRow';
+
+it('if isHeader is true, renders one cell with colspan = 2 when textSecondCell does not exist', () => {
+  const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="stuff"/>);
+  expect(wrapper.find('th').length).toBe(1);
+  expect(wrapper.find('th').find('[colSpan="2"]').length).toBe(1);
+});
+
+it('if isHeader is true, renders two cells when textSecondCell exists', () => {
+  const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="Foo" textSecondCell="Bar" />);
+  expect(wrapper.find('th').length).toBe(2);
+});
+
+it('if isHeader is false, renders two td within a tr element', () => {
+  const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="stuff"/>);
+  const trElem = wrapper.find('tr');
+  expect(trElem.length).toBe(1);
+  expect(trElem.find('td').length).toBe(2);
+});
