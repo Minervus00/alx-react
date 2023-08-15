a70ad4fee415bf70a01e08d4d4fe521b9fb07a46
Minervus00 <fayouedraogo@protonmail.com>
2023-08-15 22:52:24 +0000

    funn

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -3,10 +3,3 @@ import { Map } from "immutable";
 export default function accessImmutableObject(object, array) {
   return Map(object).getIn(array);
 }
-
-// console.log(accessImmutableObject({
-//   name: {
-//        first: "Guillaume",
-//        last: "Salva"
-//   }
-// }, ['name', 'first']));
