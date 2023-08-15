dadf8632597145d0afd68feff89be22311f9035b
Minervus00 <fayouedraogo@protonmail.com>
2023-08-15 23:08:28 +0000

    funn

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,7 +1,5 @@
-import { fromJS } from "immutable";
+import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  const obj = fromJS(object);
-
-  return obj.getIn(array, undefined);
+  return fromJS(object).getIn(array, undefined);
 }
