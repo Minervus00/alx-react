000178eeac64ac24cd7e3f28d029bfabc05d58ac
Minervus00 <fayouedraogo@protonmail.com>
2023-08-16 16:33:51 +0000

    funny

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,15 +1,19 @@
 import { Seq } from 'immutable';
 
-function capitalize(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
+
 
 export default function printBestStudents(grades) {
   const lazySeq = Seq(grades);
   const bestJS = lazySeq.filter(student => student.score >= 70).toJS();
+  
+  function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
   Object.keys(bestJS).forEach(key => {
     bestJS[key]['firstName'] = capitalize(bestJS[key]['firstName']);
     bestJS[key]['lastName'] = capitalize(bestJS[key]['lastName']);
   })
+
   console.log(bestJS);
 }
