b3783e0cef326a960feee3018575e2eaa036ab8c
Minervus00 <fayouedraogo@protonmail.com>
2023-08-16 11:55:20 +0000

    funny

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,7 +1,7 @@
-import { Map } from "immutable";
+import { Map } from 'immutable';
 
 function mergeDeeplyElements(page1, page2) {
-
+  return Map(page1).mergeDeep(page2);
 }
 
 const page1 = {
@@ -26,4 +26,4 @@ const page2 = {
   },
 };
 
-mergeDeeplyElements(page1, page2).toJS();
+// console.log(mergeDeeplyElements(page1, page2).toJS());
