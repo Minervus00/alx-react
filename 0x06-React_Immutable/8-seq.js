d64e400e36076f0e3cf6a6463d001ae51f41171c
Minervus00 <fayouedraogo@protonmail.com>
2023-08-16 16:22:24 +0000

    funny

---
+++ 0x06-React_Immutable/8-seq.js
@@ -0,0 +1,31 @@
+import { Seq, Map } from 'immutable';
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export default function printBestStudents(grades) {
+  const lazySeq = Seq(Map(grades));
+  const bestJS = lazySeq.filter(student => student.score >= 70).toJS();
+  console.log(bestJS['1']['firstName']);
+  Object.keys(bestJS).forEach(key => {
+    bestJS[key]['firstName'] = capitalize(bestJS[key]['firstName']);
+    bestJS[key]['lastName'] = capitalize(bestJS[key]['lastName']);
+  })
+  console.log(bestJS);
+}
+
+// const grad = {
+//   1: {
+//     score: 99,
+//     firstName: 'guillaume',
+//     lastName: 'salva',
+//   },
+//   2: {
+//     score: 109,
+//     firstName: 'julien',
+//     lastName: 'barbier',
+//   }
+// };
+
+// printBestStudents(grad);
