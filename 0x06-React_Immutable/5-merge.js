369bb607189c8d83dce102a84b7060ae0f63aa99
Minervus00 <fayouedraogo@protonmail.com>
2023-08-16 11:48:04 +0000

    funn

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,15 +1,11 @@
 import { List, Map } from 'immutable';
 
 export function concatElements(page1, page2) {
-  return List(page1).merge(page2);
+  return List(page1).concat(page2);
 }
 
 export function mergeElements(page1, page2) {
-  const val1 = Object.values(page1);
-  const val2 = Object.values(page2);
-  if (!Map(page1).equals(Map(page2))) {
-    return concatElements(val1, val2);
-  } else {
-    return List(val2);
-  }
+  const m1 = Map(page1);
+  const m2 = Map(page2);
+  return m1.merge(m2);
 }
