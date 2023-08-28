83c1f43afde4f9e85b7ea9ebea3b86ebd0afea19
Minervus00 <fayouedraogo@protonmail.com>
2023-08-28 16:31:30 +0000

    fun

---
+++ 0x08-react_redux_reducer_selector/task_5/dashboard/src/selectors/notificationSelector.js
@@ -0,0 +1,9 @@
+import Map from 'immutable';
+
+export const filterTypeSelected = (state) => state.get('filter');
+export const getNotifications = (state) => Object.values(state.get('notifications'));
+
+export function getUnreadNotifications(state) {
+  const notifs = getNotifications(state);
+  return notifs.filter((value) => value.isRead === false);
+}
